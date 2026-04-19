62c141c5f0620fc7e0790fbaaa06711018173458
Nimira43 <n.rabone72@aol.com>
2026-04-19 02:17:36 +0100

    Extra course data.

--- components/data/uiData.js
+++ components/data/uiData.js
@@ -17,5 +17,9 @@ export const courses = [
   { id: 1, title: 'Reactive Accelerator', category: 'Development', chapters: 4, price: 49 },
   { id: 2, title: 'Creative Design Mastery', category: 'Design', chapters: 6, price: 59 },
   { id: 3, title: 'Marketing Strategy 101', category: 'Marketing', chapters: 5, price: 39 },
-  { id: 4, title: 'Business Foundations', category: 'Business', chapters: 8, price: 69 },
+  { id: 4, title: 'IT Foundations', category: 'IT & Software', chapters: 8, price: 69 },
+  { id: 5, title: 'Communications', category: 'Personal Development', chapters: 3, price: 59 },
+  { id: 6, title: 'Photo Mastery', category: 'Photography', chapters: 8, price: 69 },
+  { id: 7, title: 'Sound & Acoustics', category: 'Music', chapters: 6, price: 59 },
+  { id: 8, title: 'Business Foundations', category: 'Business', chapters: 7, price: 49 },
 ]
