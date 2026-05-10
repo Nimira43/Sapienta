fbd7bad9c6726e996ebb0c04cb7e493575646152
Nimira43 <n.rabone72@aol.com>
2026-05-10 23:37:46 +0100

    Login, register and avatar navlinks created.

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -1,5 +1,10 @@
-import { cn } from "@/lib/utils"
-import Link from "next/link"
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { VscMenu } from 'react-icons/vsc'
+import { LiaTimesSolid } from 'react-icons/lia'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 function MainNav({items, children}) {
   return (
@@ -35,8 +40,97 @@ function MainNav({items, children}) {
             : null
         }
       </div>
-    </>
-    
+      <nav className='flex items-center gap-3'>
+        <div className='items-center gap-3 hidden lg:flex'>
+          <Link
+            href='/login'
+            className='px-4 font-medium hover:text-main transitioning'
+          >
+            Login          
+          </Link>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant='link'
+              >
+                Register
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+              align='end'
+              className='w-56 mt-4'
+            >
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+                <Link href='#'>
+                  Student
+                </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+                <Link href='#'>
+                  Instructor
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <div className='cursor-pointer'>
+              <Avatar>
+                <AvatarImage
+                  src='/images/user.jpg'
+                  alt="User"
+                />
+                <AvatarFallback>LB</AvatarFallback>
+              </Avatar>
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent
+              align='end'
+              className='w-56 mt-4'
+            >
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+              <Link href='/account'>
+                Profile
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+              <Link href='/account/enrolled-courses'>
+                My Courses
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+              <Link href='#'>
+                Testimonials & Certificates
+              </Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem 
+                className='cursor-pointer'
+                asChild
+              >  
+              <Link href='#'>
+                Logout
+              </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+      </nav>
+    </>   
   )
 }
 
